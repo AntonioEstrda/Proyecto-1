cba39b3724f9ba69077269dd8a616bcb418fb66a
UnSup17 <collazosdie@unicauca.edu.com.co>
2022-11-25 13:08:26 -0600

    navbar y footer

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,20 @@ import ResourceApp from "./apps/AppResource";
 import "./index.css";
 import { FacultyContextProvider } from "./context/FacultyContext";
 import { ResourceContextProvider } from "./context/ResourceContext";
+import Navbar from "./templates/Navbar";
+import Footer from "./templates/Footer";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div>
-      <div className="grid grid-cols-2">
-        <FacultyContextProvider>
-          <FacultyApp />
-        </FacultyContextProvider>
-        <ResourceContextProvider>
-          <ResourceApp />
-        </ResourceContextProvider>
-      </div>
+    <Navbar />
+    <div className="grid grid-cols-2">
+      <FacultyContextProvider>
+        <FacultyApp />
+      </FacultyContextProvider>
+      <ResourceContextProvider>
+        <ResourceApp />
+      </ResourceContextProvider>
     </div>
+    <Footer />
   </React.StrictMode>
 );
