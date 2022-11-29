866af1e37f9b439840516292980b8d05ab581ce5
Andrés David Muñoz Cerón <90346628+Tuldrum@users.noreply.github.com>
2022-11-28 21:53:13 -0500

    disabled CORS

--- Server/src/main/java/server/server/Controller/WebConfigurer.java
+++ Server/src/main/java/server/server/Controller/WebConfigurer.java
@@ -4,26 +4,20 @@
  */
 package server.server.Controller;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  *
  * @author anmon
  */
 @Configuration
-public class WebConfigurer {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-            }
-        };
+public class WebConfigurer implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
+    
 }
