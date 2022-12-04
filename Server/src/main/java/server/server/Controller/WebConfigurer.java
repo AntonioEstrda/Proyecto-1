68a08eddb4488b5186d9c37a485dfc4c0d994f52
Andrés David Muñoz Cerón <90346628+Tuldrum@users.noreply.github.com>
2022-12-03 19:35:07 -0500

    CORS CRFS

--- Server/src/main/java/server/server/Controller/WebConfigurer.java
+++ Server/src/main/java/server/server/Controller/WebConfigurer.java
@@ -15,6 +15,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfigurer implements WebMvcConfigurer {
     
-   
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
     
 }
--- Server/src/main/java/server/server/Controller/WebSecurityConfig.java
+++ Server/src/main/java/server/server/Controller/WebSecurityConfig.java
@@ -4,19 +4,58 @@
  */
 package server.server.Controller;
 
+import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableWebSecurity
-public class WebSecurityConfig{
-       @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // ...
-        http.cors().disable();
-        http.csrf().disable();  
-        return http.build();
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.cors();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+
+        ArrayList<String> strs = new ArrayList();
+        ArrayList<String> str2 = new ArrayList();
+        ArrayList<String> str3 = new ArrayList();
+
+        strs.add("*");
+
+        str2.add("HEAD");
+        str2.add("POST");
+        str2.add("GET");
+        str2.add("PUT");
+        str2.add("DELETE");
+        str2.add("PATCH");
+
+        str3.add("Authorization");
+
+        str3.add("Cache-Control");
+
+        str3.add("Content-Type");
+
+        configuration.setAllowedOrigins(strs);
+        configuration.setAllowedMethods(str2);
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(str3);
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
