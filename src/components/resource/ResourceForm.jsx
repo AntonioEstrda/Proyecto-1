919288d65f044fd3ffc5ed196e086d3adbee4acd
Javier Ramos <jedelgado1404@gmail.com>
2022-12-06 08:22:00 -0500

    Adecuación Resource form

--- src/components/resource/ResourceForm.jsx
+++ src/components/resource/ResourceForm.jsx
@@ -6,6 +6,11 @@ function ResourceForm() {
   const [description, setDescription] = useState("");
   const [resourceType, setResourceType] = useState("computacional");
   const [disable, setDisable] = useState(false);
+  const [code, setCode] = useState("");
+  const [location, setLocation] = useState("");
+  const [capacity, setCapacity] = useState();
+  const [number, setNumber] = useState();
+
 
   const { createResource } = useContext(ResourceContext);
 
@@ -16,12 +21,17 @@ function ResourceForm() {
     setDescription("");
     setResourceType("computacional");
     setDisable(false);
+    setCode("");
+    setLocation("");
+    setCapacity(0);
+    setNumber(0);
   };
 
   return (
     <div className="max-w-md mx-auto">
       <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4 ">
-        <h1 className="text-2xl font-bold text-white mb-3">Crear un Recurso</h1>
+        <h1 className="text-2xl font-bold text-white mb-3">
+          Crear un Recurso</h1>
         <input
           placeholder="Nombre del recurso"
           onChange={(e) => setName(e.target.value)}
@@ -54,6 +64,30 @@ function ResourceForm() {
           <option value="false">Activo</option>
           <option value="true">Inactivo</option>
         </select>
+        <input
+          placeholder="Código del recurso"
+          onChange={(e) => setCode(e.target.value)}
+          className="bg-slate-200 p-3 w-full mb-2"
+          value={code}
+        />
+        <input
+          placeholder="Locación"
+          onChange={(e) => setLocation(e.target.value)}
+          className="bg-slate-200 p-3 w-full mb-2"
+          value={location}
+        />
+        <input
+          placeholder="Capacidad"
+          onChange={(e) => setCapacity(e.target.value)}
+          className="bg-slate-200 p-3 w-full mb-2"
+          value={capacity}
+        />
+        <input
+          placeholder="Número del recurso"
+          onChange={(e) => setNumber(e.target.value)}
+          className="bg-slate-200 p-3 w-full mb-2"
+          value={number}
+        />
         <button className="bg-indigo-500 px-3 py-1 text-white">Guardar</button>
       </form>
     </div>
