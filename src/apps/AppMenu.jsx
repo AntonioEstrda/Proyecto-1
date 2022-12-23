574a080fdecd303eac29ddfd7b38d2400c7c6e6a
UnSup17 <collazosdie@unicauca.edu.com.co>
2022-12-22 20:40:23 -0600

    Main menu sin fragmentar en componentes

---
+++ src/apps/AppMenu.jsx
@@ -0,0 +1,178 @@
+import { Link } from "react-router-dom";
+
+function AppMenu() {
+  return (
+    <>
+      <div className="h-full flex flex-col bg-gray-100 dark:bg-gray-700 shadow-xl">
+        <div className="bg-green-300 shadow-lg pb-3 rounded-b-3xl">
+          <div className="flex  rounded-b-3xl bg-gray-100 dark:bg-gray-700 space-y-5 flex-col items-center py-7">
+            <img
+              className="h-28 w-28 rounded-full"
+              src="https://api.lorem.space/image/face?w=120&h=120&hash=bart89fe"
+              alt="User"
+            ></img>
+          </div>
+          <div className="grid px-7 pt-2  items-center justify-around grid-cols-2 gap-4 divide-x divide-solid">
+            <div className="col-span-1 flex flex-col items-center  text-center">
+              <Link
+                to="/home"
+                className="text-2xl font-bold dark:text-gray-500"
+              >
+                Tu Perfil
+              </Link>
+            </div>
+            <div className="col-span-1 px-3 flex flex-col items-center ">
+              <Link
+                to="/home"
+                className="text-2xl font-bold dark:text-gray-500"
+              >
+                Configuraciones
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="m-4 mt-7 bg-slate-900 rounded-2xl">
+          <div className="text-2xl font-bold dark:text-slate-300 m-5">
+            Gestionar horario
+          </div>
+          <div className="grid divide-y divide-x divide-dashed hover:divide-solid  justify-evenly bg-gray-50 dark:bg-gray-300 m-5 grid-cols-2">
+            <div className="col-span-1 p-3">
+              <div className="flex flex-col items-center ">
+                <Link to="/home">
+                  <button className="tr-300 text-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-14 w-14 text-gray-500"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                      />
+                    </svg>
+                    <span className="text-lg font-medium">
+                      Importar horario
+                    </span>
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <div className="col-span-1  p-3">
+              <div className="flex flex-col items-center ">
+                <Link to="/horarios">
+                  <button className="tr-300">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-14 w-14 text-gray-500"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                      />
+                    </svg>
+                    <span className="text-lg font-medium">
+                      Horarios cargados
+                    </span>
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="m-4 mb-7 bg-slate-900 rounded-2xl">
+          <div className="text-2xl font-bold dark:text-slate-300 m-5">
+            Gestionar datos
+          </div>
+          <div className="grid divide-y divide-x divide-dashed hover:divide-solid  justify-evenly bg-gray-50 dark:bg-gray-300 m-5 grid-cols-3">
+            <div className="col-span-1 p-3">
+              <div className="flex flex-col items-center ">
+                <Link to="/facultades">
+                  <button className="tr-300">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-14 w-14 text-gray-500"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                      />
+                    </svg>
+                    <span className="text-lg font-medium">Facultades</span>
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <div className="col-span-1  p-3">
+              <div className="flex flex-col items-center ">
+                <Link to="/recursos">
+                  {" "}
+                  <button className="tr-300">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-14 w-14 text-gray-500"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                      />
+                    </svg>
+                    <span className="text-lg font-medium">Recursos</span>
+                  </button>
+                </Link>
+              </div>
+            </div>
+            <div className="col-span-1  p-3">
+              <div className="flex flex-col items-center ">
+                <Link to="/recursosTypes">
+                  {" "}
+                  <button className="tr-300">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-14 w-14 text-gray-500"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                      />
+                    </svg>
+                    <span className="text-lg font-medium">
+                      Tipos de recursos
+                    </span>
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AppMenu;
--- src/templates/Navbar.jsx
+++ src/templates/Navbar.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="bg-blue text-bone border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-blue">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <Link to="/" className="flex items-center">
+        <Link to="/home" className="flex items-center">
           <img
             src="https://revistas.unicauca.edu.co/public/site/images/logofooter__1__a69a7fd2c99533ac11ab53b42ae1d434.png"
             className="h-7 mr-3 sm:h-16"
@@ -17,7 +17,7 @@ export default function Navbar() {
           <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-blue dark:border-gray-700">
             <li>
               <Link
-                to="/"
+                to="/home"
                 className="block py-2 pl-3 pr-4 text-bone bg-blue-700 rounded md:bg-transparent md:text-bone md:p-0 dark:text-white"
                 aria-current="page"
               >
@@ -26,32 +26,10 @@ export default function Navbar() {
             </li>
             <li>
               <Link
-                to="/horarios"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-bone dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Horarios
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/facultades"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-bone dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Facultades
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/recursos"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-bone dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Recursos
-              </Link>
-              <Link
-                to="/recursosTypes"
+                to="/home"
                 className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Tipo de Recursos
+                Cerrar sesión
               </Link>
             </li>
           </ul>
