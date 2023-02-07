a4fd183e703a5210990cfba7d3e38cc15598215b
Javier Ramos <jedelgado1404@gmail.com>
2023-02-07 16:05:59 -0500

    cms Event (problems)

--- src/components/event/EventCard.jsx
+++ src/components/event/EventCard.jsx
@@ -2,9 +2,7 @@ import { useContext } from "react";
 import { EventContext } from "../../context/EventContext";
 
 export default function EventCard({ event }) {
-  const { deleteById, setEditingEvent } = useContext(
-    EventContext
-  );
+  const { deleteById, setEditingEvent } = useContext(EventContext);
 
   function defineEditItem() {
     setEditingEvent(event);
@@ -13,17 +11,28 @@ export default function EventCard({ event }) {
   return (
     <div className="bg-paleta2-purpura text-paleta2-azul-claro p-4 rounded-md">
       <h1 className="text-xl font-bold capitalize">{event?.name}</h1>
-      <p className="text-paleta2-azul-claro text-sm">
-        Fecha inicio: {event?.initDate}
-      </p>
-      <p className="text-paleta2-azul-claro text-sm">
-        Fecha final: {event?.finalDate}
-      </p>
+      <h1 className="text-xl font-bold capitalize">{event?.description}</h1>
+      <h1 className="text-xl font-bold capitalize">{event?.code}</h1>
+      <h1 className="text-xl font-bold capitalize">{event?.type}</h1>
+
+      <div className="bg-paleta2-azulverd text-paleta2-naranja p-4 mt-2 rounded-md">
+        <p className="text-lg font-bold text-blue">Profesor:</p>
+        {event.teacher?.firstName}
+      </div>
+
+      <div className="bg-paleta2-azulverd text-paleta2-naranja p-4 mt-2 rounded-md">
+        <p className="text-lg font-bold text-blue">Periodo AC:</p>
+        {event.academicPeriod?.name}
+      </div>
+      <div className="bg-paleta2-azulverd text-paleta2-naranja p-4 mt-2 rounded-md">
+        <p className="text-lg font-bold text-blue">Departamento:</p>
+        {event.department?.name}
+      </div>
 
       <div className="grid grid-cols-2 gap-3">
         <button
           className="bg-paleta2-morado px-2 py-1 rounded-md mt-4 hover:bg-red-400"
-          onClick={() => deleteById(event?.eventID)}
+          onClick={() => deleteById(event?.id, event?.department?.departmentId)}
         >
           Eliminar
         </button>
--- src/components/event/EventList.jsx
+++ src/components/event/EventList.jsx
@@ -8,18 +8,15 @@ export default function EventList() {
   if (events.length === 0) {
     return (
       <h1 className="text-white text-4xl font-bold text-center">
-        No hay Periodos Académicos registrados
+        No hay Eventos registrados
       </h1>
     );
   }
 
   return (
     <div className="grid grid-cols-4 gap-2">
-      {events.map((events) => (
-        <EventCard
-          key={events.eventID}
-          event={events}
-        />
+      {events.map((event) => (
+        <EventCard key={event.id} event={events} />
       ))}
     </div>
   );
