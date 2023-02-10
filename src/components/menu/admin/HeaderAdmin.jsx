29576edc3307ee5080e566e1cf59cd3ef1f417a7
UnSup17 <collazosdie@unicauca.edu.com.co>
2023-02-10 17:02:01 -0600

    foto de usuario anonima

--- src/components/menu/admin/HeaderAdmin.jsx
+++ src/components/menu/admin/HeaderAdmin.jsx
@@ -6,18 +6,24 @@ function HeaderAdmin() {
       <div className="flex  rounded-b-3xl bg-gray-100 dark:bg-paleta2-morado space-y-5 flex-col items-center py-7">
         <img
           className="h-28 w-28 rounded-full"
-          src="https://api.lorem.space/image/face?w=120&h=120&hash=bart89fe"
+          src="https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
           alt="User"
         ></img>
       </div>
       <div className="grid px-7 pt-2  items-center justify-around grid-cols-2 gap-4 divide-x divide-solid">
         <div className="col-span-1 flex flex-col items-center  text-center">
-          <Link to="/home" className="text-2xl font-bold dark:text-paleta2-cafe">
+          <Link
+            to="/home"
+            className="text-2xl font-bold dark:text-paleta2-cafe"
+          >
             Tu Perfil
           </Link>
         </div>
         <div className="col-span-1 px-3 flex flex-col items-center ">
-          <Link to="/home" className="text-2xl font-bold dark:text-paleta2-cafe">
+          <Link
+            to="/home"
+            className="text-2xl font-bold dark:text-paleta2-cafe"
+          >
             Configuraciones
           </Link>
         </div>
